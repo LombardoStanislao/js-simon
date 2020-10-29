58a47360044fb116b42b9d2637fefdd63a88938d
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-10-29 18:42:46 +0100

    Sistemati i cicli ed aggiunto controlli sull'esercizio

--- main.js
+++ main.js
@@ -5,9 +5,10 @@
 
 // BONUS: visualizzare in pagina anche un timer con il countdown dei 30 secondi
 
-
+// Creo la variabile contenente i secondi di countdown
 var countdown = 30;
 
+// Creo l'intervallo di tempo in cui deve stampar a pagina il countdown
 var timeout = setInterval(function() {
 
     $('#countdown-numbers').text(countdown);
@@ -22,54 +23,96 @@ var timeout = setInterval(function() {
 
 $(document).ready(function() {
 
+// Variabile utile in futuro se aggiungerò la possibilità di chieder all'utente
+// quanti millisecondi vuole aspettare
+
     var millisecondi = 30 * 1000;
 
+// Array che riempirò con i numeri casuali dati dal pc
     var arrayNumbers = [];
 
+// Array che riempierò con i numeri inseriti dall'utente
     var userNumbers = [];
 
+// Array che riempirò con i numeri indovinati dall'utente
     var numbersRight = [];
 
+// Variabile che conterà quanti numeri inseriti sono corretti
     var matchedNumbers = 0;
 
-    for (var i = 0; i < 5; i++) {
+
+// Ciclo while per far inserire al pc esattamente 5 numeri diversi tra loro
+    while (arrayNumbers.length < 5) {
 
         var randomNumbers = getRndInteger(1, 100);
 
-        arrayNumbers.push(randomNumbers);
+// Controllo che i numeri siano tutti diversi
+        if (arrayNumbers.includes(randomNumbers)) {
 
-        console.log(randomNumbers);
+        } else {
+            arrayNumbers.push(randomNumbers);
 
-        $('.numbers').append('<p>' + randomNumbers + '</p>');
+            console.log(randomNumbers);
+
+// Stampo in pagina il numero inserito dal pc
+
+            $('.numbers').append('<p>' + randomNumbers + '</p>');
+        }
+
+
     }
 
-
+// Timeout che fa sparire i numeri del pc
     setTimeout(function() {
 
         $('.numbers').hide();
 
     }, millisecondi + 1000);
 
+// Timeout che imposta i secondi dopo cui l'utente deve inserir i numeri
     setTimeout(function() {
 
-        for (var i = 0; i < 5; i++) {
 
+// Ciclo che fa inserir i numeri all'utente finchè il suo array non sarà di 5
+
+        do {
             var numberChoise =  Math.floor(parseInt(prompt('Inserisci un numero da 1 a 100')));
-            userNumbers.push(numberChoise);
 
-            if (arrayNumbers.includes(numberChoise)) {
+// Controllo che sia un numbero
 
-                matchedNumbers += 1;
+            if (isNaN(numberChoise)) {
+                alert('Devi inserire un numero')
 
-                numbersRight.push(numberChoise);
+// Controllo che il numero inserito non sia uguale ad uno precedentemente
 
+            } else if (userNumbers.includes(numberChoise)) {
 
+                alert('Devi inserire solo numeri diversi fra loro')
+
+// Se va tutto bene procedo
+            } else {
+
+// Pusho il numero nell'array che contiene tutti i numeri inseriti dall'utente
+                userNumbers.push(numberChoise);
+
+// Se l'array del pc contiene il numero inserito...
+                if (arrayNumbers.includes(numberChoise)) {
+
+// Aumento la variabile matchedNumbers di 1
+                    matchedNumbers += 1;
+
+// E pusho il numero nell'array di numeri indovinati
+                    numbersRight.push(numberChoise);
+
+                }
             }
 
-        }
+// La condizione è "finchè l'array dei numeri dell'utente non arriva a 5"
+        } while (userNumbers.length < 5);
 
         console.log(matchedNumbers);
-
+        
+// Stampo in pagina i vari risultati
         $('#numbers-selected').text('I numeri che hai inserito sono: ' + userNumbers)
 
         $('#userpoint').text('Complimenti! Hai ricordato ' + matchedNumbers + ' numeri')
@@ -81,13 +124,6 @@ $(document).ready(function() {
     }, millisecondi + 2000)
 
     console.log(arrayNumbers);
-
-
-
-
-
-
-
 
 
     function getRndInteger(min, max) {
