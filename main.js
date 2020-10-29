cebf17ef1e013e6ff659c380bba8cc869e74ea12
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-10-29 15:46:55 +0100

    Per ticket

--- main.js
+++ main.js
@@ -1,4 +1,39 @@
+// Visualizzare in pagina 5 numeri casuali.
+// Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi, vengono rimossi i numeri dalla pagina e l'utente deve inserire (tramite prompt) i numeri che ha visto precedentemente, uno alla volta.
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+
+
+
+
 $(document).ready(function() {
+
+
+
+
+    var arrayNumbers = [];
+
+    for (var i = 0; i < 5; i++) {
+
+        var randomNumbers = $('.numbers').append(getRndInteger(1, 100));
+
+        arrayNumbers.push(randomNumbers);
+
+        console.log(randomNumbers);
+
+
+    }
+
+    console.log(arrayNumbers);
+
+
+
+
+    function getRndInteger(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
 
 
 });
